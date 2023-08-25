1f205e8b7a523e012eed6eb4b6bb7953367b2fd9
TimeLess613 <83859308+TimeLess613@users.noreply.github.com>
2023-08-25 18:04:33 +0900

    Update workflow-scan4HTB.sh
    change info style

--- workflow-scan4HTB.sh
+++ workflow-scan4HTB.sh
@@ -1,59 +1,58 @@
 #!/usr/bin/bash
 # -*- coding:utf-8 -*-
 
-## Usage: sudo bash ~/workflow-scan4HTB/workflow-scan4HTB.sh <HTB_IP>
+## Usage: sudo bash workflow-scan4HTB.sh <HTB_IP>
 
 set -eu
 
 HTB_IP=${1}
-YT_BB='\033[33;44m'    # yellow text blue background
-RS='\033[0m'           # reset style
+info_YT_BB='\033[33;44m[info]\033[0m'    # yellow text blue background
 
 
-echo -e "${YT_BB}[info]${RS} Starting ports scan..."
+echo -e "${info_YT_BB} Starting ports scan..."
 nmap -Pn -n -sT -p- --min-rate=10000 ${HTB_IP} | tee "${HTB_IP}_ports_all.nmap"
 
-echo -e "${YT_BB}[info]${RS} Starting base scan..."
+echo -e "${info_YT_BB} Starting base scan..."
 ports=$(grep 'open' "${HTB_IP}_ports_all.nmap" | cut -d '/' -f1 | paste -sd ',')
 nmap -v -Pn -n -sT --reason -sV -O -p ${ports} ${HTB_IP} | tee "${HTB_IP}_baseScan.nmap"
-echo -e "${YT_BB}[info]${RS} Base scan is Done."
+echo -e "${info_YT_BB} Base scan is Done."
 
-echo -e "${YT_BB}[info]${RS} Starting sC & vuln scan for ports < 5000..."
+echo -e "${info_YT_BB} Starting sC & vuln scan for ports < 5000..."
 ports_lt5000=$(grep 'open' "${HTB_IP}_ports_all.nmap" | cut -d '/' -f1 | awk '$1 < 5000' | paste -sd ',')
 nohup nmap -v -Pn -n -p ${ports_lt5000} --script=vuln ${HTB_IP} > "${HTB_IP}_NSE-vuln.nmap" 2>&1 &
-echo -e "${YT_BB}[info]${RS} Running NSE vuln scan background..."
+echo -e "${info_YT_BB} Running NSE vuln scan background..."
 
-echo -e "${YT_BB}[info]${RS} ==============================================="
+echo -e "${info_YT_BB} ==============================================="
 
-echo -e "${YT_BB}[info]${RS} Checking if there is domain for add to hosts..."
+echo -e "${info_YT_BB} Checking if there is domain for add to hosts..."
 HEADER_Location=$(curl -m 3 -I ${HTB_IP} | grep "Location:" || true)
 
 if [[ ${HEADER_Location} != '' ]];then
   HTB_DOMAIN=$(echo ${HEADER_Location} | cut -d '/' -f 3)
-  echo -e "${YT_BB}[info]${RS} HTB_DOMAIN: ${HTB_DOMAIN}"
+  echo -e "${info_YT_BB} HTB_DOMAIN: ${HTB_DOMAIN}"
 
   if [[ ${HTB_DOMAIN} != '' ]];then
     # shows for backup
-    echo -e "${YT_BB}[info]${RS} -------- Back up hosts --------"
+    echo -e "${info_YT_BB} -------- Back up hosts --------"
     cat -e /etc/hosts
-    echo -e "${YT_BB}[info]${RS} -------- Backed up hosts --------"
+    echo -e "${info_YT_BB} -------- Backed up hosts --------"
 
-    echo -e "${YT_BB}[info]${RS} -------- Add HTB_DOMAIN(${HTB_DOMAIN}) to hosts --------"
+    echo -e "${info_YT_BB} -------- Add HTB_DOMAIN(${HTB_DOMAIN}) to hosts --------"
     echo "${HTB_IP}    ${HTB_DOMAIN}" >> /etc/hosts
-    echo -e "${YT_BB}[info]${RS} -------- Modified hosts --------"
+    echo -e "${info_YT_BB} -------- Modified hosts --------"
 
-    echo -e "${YT_BB}[info]${RS} -------- Show now hosts --------"
+    echo -e "${info_YT_BB} -------- Show now hosts --------"
     cat -e /etc/hosts
 
-#     echo -e "${YT_BB}[info]${RS} Scanning subdomain..."
+#     echo -e "${info_YT_BB} Scanning subdomain..."
 #     gobuster vhost -u ${HTB_DOMAIN} -w /usr/share/wordlists/amass/bitquark_subdomains_top100K.txt -t 100 --append-domain -o "subdomains_${HTB_DOMAIN}.txt"
-#     echo -e "${YT_BB}[info]${RS} Subdomain scan is Done."
+#     echo -e "${info_YT_BB} Subdomain scan is Done."
   fi
 fi
 
-echo -e "${YT_BB}[info]${RS} ==============================================="
-echo -e "${YT_BB}[info]${RS} The NSE-vuln scan maybe still running..."
-echo -e "${YT_BB}[info]${RS} Show ps..."
+echo -e "${info_YT_BB} ==============================================="
+echo -e "${info_YT_BB} The NSE-vuln scan maybe still running..."
+echo -e "${info_YT_BB} Show ps..."
 ps -ef | grep 'nmap -v '
-echo -e "${YT_BB}[info]${RS} ==============================================="
-echo -e "${YT_BB}[info]${RS} If it's still running, please check status with command: ps -ef | grep 'nmap -v -Pn -n'"
+echo -e "${info_YT_BB} ==============================================="
+echo -e "${info_YT_BB} If it's still running, please check status with command: ps -ef | grep 'nmap -v -Pn -n'"
