c40014e54901eb41f43d4bfc81865b682ea99597
TimeLess613 <83859308+TimeLess613@users.noreply.github.com>
2023-09-03 11:01:30 +0900

    mv the reason option
    because the base scan just for open ports

--- workflow-scan4HTB.sh
+++ workflow-scan4HTB.sh
@@ -10,11 +10,11 @@ info_YT_BB='\033[33;44m[info]\033[0m'    # yellow text blue background
 
 
 echo -e "${info_YT_BB} Starting ports scan..."
-nmap -Pn -n -sT -p- --min-rate=10000 ${HTB_IP} | tee "${HTB_IP}_ports_all.nmap"
+nmap -Pn -n -sT --reason -p- --min-rate=10000 ${HTB_IP} | tee "${HTB_IP}_ports_all.nmap"
 
 echo -e "${info_YT_BB} Starting base scan..."
 ports=$(grep 'open' "${HTB_IP}_ports_all.nmap" | cut -d '/' -f1 | paste -sd ',')
-nmap -v -Pn -n -sT --reason -sV -O -p ${ports} ${HTB_IP} | tee "${HTB_IP}_baseScan.nmap"
+nmap -v -Pn -n -sT -sV -O -p ${ports} ${HTB_IP} | tee "${HTB_IP}_baseScan.nmap"
 echo -e "${info_YT_BB} Base scan is Done."
 
 echo -e "${info_YT_BB} Starting sC & vuln scan for ports < 5000..."
