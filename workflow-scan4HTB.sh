7419be55d5cbf306064d03bfc592e85697b5f23f
TimeLess613 <83859308+TimeLess613@users.noreply.github.com>
2024-05-29 14:28:31 +0900

    fix. miss fi.

--- workflow-scan4HTB.sh
+++ workflow-scan4HTB.sh
@@ -61,3 +61,4 @@ else
   echo -e "${info_YT_BB} If it's still running, please check status with command: ps -ef | grep 'nmap -v -Pn -n'"
   ps -ef | grep 'nmap -v -Pn -n'
   echo -e "${info_YT_BB} Maybe next step for dir-enum with feroxbuster/gobuster/fuff: feroxbuster -u http://${HTB_IP}/ -w /usr/share/wordlists/dirb/big.txt -x php,txt"
+fi
