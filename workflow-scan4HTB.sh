b1410a28cdaf5206820e8bc2ddf5b2da28ac1a16
TimeLess613 <83859308+TimeLess613@users.noreply.github.com>
2024-08-10 00:49:46 +0900

    add work_dir

--- workflow-scan4HTB.sh
+++ workflow-scan4HTB.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/bash
 # -*- coding:utf-8 -*-
 
-## Usage: sudo bash workflow-scan4HTB.sh <HTB_IP>
+## Usage: sudo bash workflow-scan4HTB.sh <HTB-MachineName> <HTB_IP>
 
 set -eu
 
-HTB_IP=${1}
+WORK_DIR="/home/kali/HTB/${1}/infoG/initscan"
+HTB_IP=${2}
+
 info_YT_BB='\033[33;44m[info]\033[0m'    # yellow text blue background
 
+echo -e "${info_YT_BB} Create and move to WORK_DIR: ${WORK_DIR}"
+mkdir -p "$WORK_DIR"
+cd $WORK_DIR
 
 echo -e "${info_YT_BB} Starting ports scan..."
 nmap -Pn -n -sT --reason -p- --min-rate=5000 ${HTB_IP} -oA "${HTB_IP}_ports_all"
@@ -16,12 +21,14 @@ nmap -Pn -n -sT --reason -p- --min-rate=5000 ${HTB_IP} -oA "${HTB_IP}_ports_all2
 file1="${HTB_IP}_ports_all.nmap"
 file2="${HTB_IP}_ports_all2.nmap"
 
-diff <(fgrep open $file1) <(fgrep open $file2)
+set +e
+diff <(fgrep open $file1 | cut -d ' ' -f1) <(fgrep open $file2 | cut -d ' ' -f1)
 diff_status=$?
-echo ${diff_status}  # for sometimes debug.
+set -e
+
 if [ ${diff_status} -eq 0 ]; then
   selected_file="$file1"
-  echo -e "${info_YT_BB} Same result files, use $selected_file to do next step..."
+  echo -e "${info_YT_BB} Same result about open ports, use $selected_file to do next step..."
 else
   echo "Select a results file to do next step:"
   echo "1) $file1"
@@ -55,14 +62,12 @@ ports=$(grep 'open' "${selected_file}" | cut -d '/' -f1 | paste -sd ',')
 nmap -v -Pn -n -sT -sV -O -p ${ports} ${HTB_IP} -oA "${HTB_IP}_baseScan"
 echo -e "${info_YT_BB} Base scan is Done."
 
-echo -e "${info_YT_BB} Starting sC & vuln scan for ports < 5000..."
+echo -e "${info_YT_BB} Starting sC & vuln scan for ports < 5000 on background..."
 ports_lt5000=$(grep 'open' "${selected_file}" | cut -d '/' -f1 | awk '$1 < 5000' | paste -sd ',')
-nohup nmap -v -Pn -n -p ${ports_lt5000} -sC ${HTB_IP} -oA "${HTB_IP}_NSE-sC" > /dev/null 2>&1 &
-nohup nmap -v -Pn -n -p ${ports_lt5000} --script=vuln ${HTB_IP} -oA "${HTB_IP}_NSE-vuln" > /dev/null 2>&1 &
-echo -e "${info_YT_BB} Running NSE vuln scan background..."
+nmap -v -Pn -n -p ${ports_lt5000} -sC ${HTB_IP} -oA "${HTB_IP}_NSE-sC" > /dev/null 2>&1 &
+nmap -v -Pn -n -p ${ports_lt5000} --script=vuln ${HTB_IP} -oA "${HTB_IP}_NSE-vuln" > /dev/null 2>&1 &
 
 echo -e "${info_YT_BB} ==============================================="
-
 echo -e "${info_YT_BB} Checking if there is domain for add to hosts..."
 HEADER_Location=$(curl -s -m 3 -I ${HTB_IP} | grep "Location:" || true)
 
@@ -87,14 +92,16 @@ if [[ ${HEADER_Location} != '' ]];then
   fi
 fi
 
+echo -e "${info_YT_BB} Running whatweb..."
 whatweb ${HTB_DOMAIN:-$HTB_IP} | tee whatweb_${HTB_DOMAIN:-$HTB_IP}.txt
 
-echo -e "${info_YT_BB} ==============================================="
-echo -e "${info_YT_BB} The NSE scan maybe still running..."
-echo -e "${info_YT_BB} Show ps..."
-ps -ef | grep 'nmap -v '
-echo -e "${info_YT_BB} ==============================================="
-echo -e "${info_YT_BB} If it's still running, please check status with command: ps -ef | grep 'nmap -v -Pn -n'"
-ps -ef | grep 'nmap -v -Pn -n'
 echo -e "${info_YT_BB} Maybe next step for dir-enum with feroxbuster/gobuster/fuff: feroxbuster -u http://${HTB_DOMAIN:-$HTB_IP}/ -t 32 -w /usr/share/wordlists/dirb/big.txt -o dirEnum_${HTB_DOMAIN:-$HTB_IP}.txt -x php,txt"
 
+echo -e "${info_YT_BB} ==============================================="
+ps -ef | grep 'nmap -v -Pn -n'
+echo -e "${info_YT_BB} The NSE scan maybe still running on background. Waiting..."
+wait
+
+mkdir nmap_other_format
+mv *.gnmap nmap_other_format
+mv *.xml nmap_other_format
